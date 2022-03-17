2b10cc6f3f0ff313018635da6b041aa14051fbbb
vrussillo <80196694+vrussillo@users.noreply.github.com>
2022-03-17 10:14:42 -0700

    removed cache

---
+++ README.md
@@ -0,0 +1,41 @@
+# Moog React Node Express Patchbook Application
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<br>
+in addition to Node, Express, JS, PosgreSQL and React Router.
+
+## Demo
+* No need to signup, you can login with the following credentials:
+
+ <br>
+ username | password     | email  
+ <br>
+testuser | testpassword | test@test.com
+
+## Homepage
+
+![Homepage](client/public/images/HomePage-SS.png)
+
+## Database Schema
+
+![Database Schema](client/public/images/Schema.png)
+
+
+## User Flow
+
+* A User can Signup/Login with a Username, Email (No Contact), and Password. Password is cryptographically hashed with bcrypt and Users are generated a jwt for authenticaion through user routes.
+
+* A User can view templates and preview audio of each patch to hear results
+
+* A User can click external links to visit Moog's website for product and MetaPatch to create patches of their own.
+
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ client/src/Routes.js
@@ -0,0 +1,129 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useState, useEffect, Fragment } from "react";
+import Account from "./pages/Account";
+import Templates from "./pages/Templates";
+import Create from "./pages/Create";
+import Home from "./pages/Home";
+import LoginForm from "./auth/LoginForm";
+import SignUpForm from "./auth/SignupForm";
+import EditForm from "./auth/EditForm";
+import Dark from "./pages/Dark";
+import LBD from "./pages/LBD";
+import MikeDean from "./pages/MikeDean";
+// import HeartButton from "./components/Favorites";
+
+function WebRoutes() {
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch("/authentication/verify", {
+        method: "POST",
+        headers: { jwt_token: localStorage.token },
+      });
+
+      const parseRes = await res.json();
+
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  const AuthSignUp = () => {
+    return !isAuthenticated ? (
+      <SignUpForm setAuth={setAuth} />
+    ) : (
+      <Navigate to="/login" />
+    );
+  };
+
+  const AuthLogin = () => {
+    return !isAuthenticated ? (
+      <LoginForm setAuth={setAuth} />
+    ) : (
+      <Navigate to="/account" />
+    );
+  };
+
+  const AuthEditForm = (children) => {
+    return isAuthenticated ? (
+      <EditForm {...children} setAuth={setAuth} />
+    ) : (
+      <Navigate to="/login" />
+    );
+  };
+
+  const AuthAccount = (children) => {
+    return isAuthenticated ? (
+      <Account {...children} setAuth={setAuth} />
+    ) : (
+      <Navigate to="/login" />
+    );
+  };
+
+  const AuthDark = (children) => {
+    return isAuthenticated ? (
+      <Dark {...children} setAuth={setAuth} />
+    ) : (
+      <Navigate to="/signup" />
+    );
+  };
+
+  const AuthTemplates = (children) => {
+    return isAuthenticated ? (
+      <Templates {...children} setAuth={setAuth} />
+    ) : (
+      <Navigate to="/signup" />
+    );
+  };
+
+  const AuthLBD = (children) => {
+    return isAuthenticated ? (
+      <LBD {...children} setAuth={setAuth} />
+    ) : (
+      <Navigate to="/signup" />
+    );
+  };
+
+  const AuthMikeDean = (children) => {
+    return isAuthenticated ? (
+      <MikeDean {...children} setAuth={setAuth} />
+    ) : (
+      <Navigate to="/signup" />
+    );
+  };
+
+  return (
+    <Fragment>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/home" element={<Home />}></Route>
+        <Route exact path="/templates" element={<AuthTemplates />}></Route>
+        <Route exact path="/templates/dark/:id" element={<AuthDark />}></Route>
+        <Route exact path="/templates/lbd/:id" element={<AuthLBD />}></Route>
+        <Route
+          exact
+          path="/templates/mikedean/:id"
+          element={<AuthMikeDean />}
+        ></Route>
+        <Route exact path="/create" element={<Create />}></Route>
+        <Route exact path="/account" element={<AuthAccount />}></Route>
+        <Route exact path="/account/:id" element={<AuthEditForm />}></Route>
+        <Route exact path="/signup" element={<AuthSignUp />}></Route>
+        <Route exact path="/login" element={<AuthLogin />}></Route>
+        {/* <Route exact path="/like" element={<HeartButton />}></Route> */}
+      </Routes>
+    </Fragment>
+  );
+}
+
+export default WebRoutes;
